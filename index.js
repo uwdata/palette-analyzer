296808a4f1053e4f0d239601d9f4659cba31ea34
Yang <yliu0@cs.washington.edu>
2017-07-15 14:47:37 -0700

    * visualize using vega

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-export {default as analyze} from './src/main.js'
+export {analyze} from './src/main.js'
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import * as vega from 'vega-scale'
-import {distanceLAB} from './colorDistance'
+import {distanceUCS, distanceRGB, distanceLAB} from './colorDistance'
 
-export default function analyze () {
-  const stride = 0.1
-  let scheme = vega.scheme('greys')
+function analyze (palette, stride) {
+  stride = stride || 0.1
+  let scheme = vega.scheme(palette)
   let diff = []
+
+  // sanity check
+  if (!scheme) {
+    return diff
+  }
 
   // vega.scheme returns an array for categorical color scale and a function
   // for continuous scale; so here we are converting everything to an array
@@ -17,8 +22,17 @@ export default function analyze () {
   }
 
   for (let i = 0; i < scheme.length - 1; i++) {
-    diff.push(distanceLAB(scheme[i], scheme[i + 1]))
+    let tuple = {
+      palette: palette,
+      x: i,
+      ucs: distanceUCS(scheme[i], scheme[i + 1]),
+      rgb: distanceRGB(scheme[i], scheme[i + 1]),
+      lab: distanceLAB(scheme[i], scheme[i + 1])
+    }
+    diff.push(tuple)
   }
 
   return diff
 }
+
+export {analyze}
